deccaa32923ea34a44c8d8ce8e1e25ee9c4bc282
Mikey Harris <mikey.harris89@gmail.com>
2016-11-04 17:57:17 -0400

    svg animations

---
+++ js/triangle_animations.js
@@ -0,0 +1,12 @@
+(function() {
+
+  window.addEventListener('resize', updateTriangles);
+
+  var updateTriangles = function() {
+    console.log("functions!")
+  }
+
+  window.addEventListener('click', function() {
+   console.log("hay")
+ })
+});
