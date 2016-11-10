e2e56d95423e94ab2571da9535c562253421aa13
Mikey Harris <mikey.harris89@gmail.com>
2016-11-10 17:59:36 -0500

    Fixed triangles

--- js/triangle_animations.js
+++ js/triangle_animations.js
@@ -3,10 +3,12 @@
 
   window.addEventListener('resize', function() {
     updateTriangles();
+    updateRight();
   });
 
   var updateTriangles = function() {
     var currentWidth = window.innerWidth;
+    $previousSize = currentWidth;
     var changeWidths = $previousSize - currentWidth;
 
     var triangle = $('.triangle'),
@@ -15,8 +17,26 @@
 
 
     var height = newWidth / Math.sqrt(3);
-    var newPath = "0 100%, 0 " + (800 - height) + "px, " + newWidth + "px 100%"
-    triangle.css({"clip-path": newPath})
+    var newPath = "0 100%, 0 " + (800 - height) + "px, " + "100% 100%";
+    var ratio = "0 1, 0 " + ((800 - height) / 1000) + ", 1 1";
+    $('polygon#left-triangle').attr('points', ratio);
+    triangle.css({"clip-path": newPath});
   }
+
+  var updateRight = function() {
+    var currentWidth = window.innerWidth;
+    var changeWidth = $previousSize - currentWidth;
+
+    var rightTriangle = $('.right-triangle'),
+        width = rightTriangle.outerWidth(),
+        newWidth = changeWidth + width,
+        height = newWidth / Math.sqrt(3),
+        newPath = "100% 100%, 0 100%, 100% " + (800 - height) + "px",
+        ratio = "1 1, 0 1, 1 " + ((800 - height) / 1000);
+
+        $('polygon#right-triangle').attr('points', ratio);
+        rightTriangle.css({"clip-path": newPath});
+  }
+
 
 }(jQuery));
